4e96d8ca54c37c02a12b01a5eb7e98f62d1a1d64
IsmaelBraz <brazismael1@gmail.com>
2022-12-08 01:18:26 -0300

    up

--- main.c
+++ main.c
@@ -3,171 +3,277 @@
 #include <stdlib.h>
 #include <time.h>
 #include <conio.h>
-#include "mylib.h" 
-
+#include "mylib.h"
 
 main()
 {
-    int i = 0,j = 0,cont = 0,a = 0,sorteio, controle=0;
+    int modo, vencedor;
+    int i = 0, j = 0, cont = 0, a = 0, sorteio, controle = 0;
     char jogo[3][3];
     char user1[15], user2[15];
-    char primeiro[15], segundo[15];
+    char user3[15] = "Computador";
     char reiniciar;
-    int pont1 = 0, pont2 =0;
-    cleanmtrz(jogo);//function para limpar a matriz
-    printf("Digite o nome do primeiro jogador: ");
-    scanf(" %s", user1);
-    setbuf(stdin, 0);
-    printf("Digite o nome do segundo jogador: ");
-    scanf(" %s", user2);
-    setbuf(stdin, 0);
+    int pont1 = 0, pont2 = 0;
 
-    //Sorteio para definir qual jogador comeca primeiro
-    srand(time(NULL));
-    sorteio = (rand() % 2);
-    if(sorteio == 0){
-        printf("%s sera o primeiro a jogar com (X) e %s sera o segundo a jogar com (O).\n\n", user1,user2);
-        for(a = 0; a<strlen(user1); a++){
-        primeiro[a] = user1[a];
-        setbuf(stdin, 0);
-        }
-        for(a = 0; a<strlen(user2); a++){
-        segundo[a] = user2[a];
-        setbuf(stdin, 0);
-        }
-    }else{
-        printf("%s sera o primeiro a jogar com (X) e %s sera o segundo a jogar com (O).\n\n", user2,user1);
-        for(a = 0; a<strlen(user2); a++){
-        primeiro[a] = user2[a];
-        setbuf(stdin, 0);
-        }
-        for(a = 0; a<strlen(user1); a++){
-        segundo[a] = user1[a];
-        setbuf(stdin, 0);
-        }
-    }
-    system("pause");
-    system("cls");
-
-    for ( cont = 0; cont < 9; )
+    do
     {
-        cont++;
-        print_tabela(jogo, primeiro,pont1,pont2,segundo);;//function que imprime o tabela do jogo
-        
-        if(cont != 11){
-            do{
-            if(cont%2 != 0){
-                printf("\nVez de %s (X). \n", primeiro);
-            }else{
-                printf("\nVez de %s (O).\n", segundo);
-            }
-
-            
-            do{
-                printf("Digite a linha: ");
-                scanf("%d", &i);
-                setbuf(stdin, 0);
-                } while ((i > 3)||(i < 1));
-    
-                do{
-                printf("Digite a coluna: ");
-                scanf("%d", &j);
-                setbuf(stdin, 0);
-                } while ((j > 3)||(j < 1));
-
-                
-                if(cont%2 != 0){              
-                    if (jogo[i-1][j-1] == ' ' )
-                    {
-                        jogo[i-1][j-1] = 'X';
-                        controle = 0;
-                        system("cls");
-                    }else{
-                        printf("\nEsta posicao ja foi preenchida.\n");
-                        controle = 1;
-                    }
-                }else{
-                    if (jogo[i-1][j-1] == ' ' )
-                    {
-                        jogo[i-1][j-1] = 'O';
-                        controle = 0;
-                        system("cls");
-                        
-                    }else{
-                        printf("\nEsta posicao ja foi preenchida.\n");
-                        controle = 1;
-                    }
-                    
-                }
-            } while(controle == 1);
-        } 
-
-		//verifica se houve um vencedor depois da quarta jogada
-        if(cont > 4)
+        printf("\n\t\tOPCOES DE JOGO\n\n\t1 - JOGAR COM UM AMIGO\n\t2 - JOGAR CONTRA A MAQUINA\n\t3 - SAIR\n\n\t=> ");
+        scanf("%d", &modo);
+        setbuf(stdin, NULL);
+        system("cls");
+        switch (modo)
         {
-            if(
-            //vencedor fechando uma linha horizontal
-            ((jogo[0][0] == 'X') && (jogo[0][1] == 'X') && (jogo[0][2] == 'X'))||   
-            ((jogo[1][0] == 'X') && (jogo[1][1] == 'X') && (jogo[1][2] == 'X'))||   
-            ((jogo[2][0] == 'X') && (jogo[2][1] == 'X') && (jogo[2][2] == 'X'))||   
-            //vencedor fechando uma linha diagonal
-            ((jogo[0][0] == 'X') && (jogo[1][0] == 'X') && (jogo[2][0] == 'X'))||   
-            ((jogo[0][1] == 'X') && (jogo[1][1] == 'X') && (jogo[2][1] == 'X'))||   
-            ((jogo[0][2] == 'X') && (jogo[1][2] == 'X') && (jogo[2][2] == 'X'))||   
-            //vencedor fechando uma linha transversal
-            ((jogo[0][0] == 'X') && (jogo[1][1] == 'X') && (jogo[2][2] == 'X'))||  
-            ((jogo[0][2] == 'X') && (jogo[1][1] == 'X') && (jogo[2][0] == 'X'))
-            )
+        case 1:
+            pont1 = 0;
+            pont2 = 0;
+            cleanmtrz(jogo); // function para limpar a matriz
+            printf("\n\tDigite o nome do primeiro jogador: ");
+            gets(user1);
+            setbuf(stdin, NULL);
+            printf("\n\tDigite o nome do segundo jogador: ");
+            gets(user2);
+            setbuf(stdin, NULL);
+            printf("\t%s ira jogar com 'X' e %s ira jogar com 'O'.\n\n", user1, user2);
+            system("pause");
+            system("cls");
+            for (cont = 0; cont < 9;)
             {
-                pont1++;
-                print_tabela(jogo, primeiro,pont1,pont2,segundo);
-                printf("\nFIM DE JOGO.\n%s VENCEU!!\n\n", primeiro);
-                do{
-                printf("\nJogar novamente? (S/N): ");
-                scanf("%c", &reiniciar); 
-                setbuf(stdin, 0); 
-                cont = reset(reiniciar, cont);//function para reiniciar ou encerrar o jogo
-                cleanmtrz(jogo);
-                }while (cont == 99);
-            }
-            if(
-            //vencedor fechando uma linha horizontal
-            ((jogo[0][0] == 'O') && (jogo[0][1] == 'O') && (jogo[0][2] == 'O'))||
-            ((jogo[1][0] == 'O') && (jogo[1][1] == 'O') && (jogo[1][2] == 'O'))||
-            ((jogo[2][0] == 'O') && (jogo[2][1] == 'O') && (jogo[2][2] == 'O'))||
-            //vencedor fechando uma linha diagonal
-            ((jogo[0][0] == 'O') && (jogo[1][0] == 'O') && (jogo[2][0] == 'O'))|| 
-            ((jogo[0][1] == 'O') && (jogo[1][1] == 'O') && (jogo[2][1] == 'O'))|| 
-            ((jogo[0][2] == 'O') && (jogo[1][2] == 'O') && (jogo[2][2] == 'O'))||
-            //vencedor fechando uma linha transversal
-            ((jogo[0][0] == 'O') && (jogo[1][1] == 'O') && (jogo[2][2] == 'O'))||
-            ((jogo[0][2] == 'O') && (jogo[1][1] == 'O') && (jogo[2][0] == 'O'))
-            )
-            {  
-                pont2++;
-                print_tabela(jogo, primeiro,pont1,pont2,segundo);
-                printf("\nFIM DE JOGO.\n%s VENCEU!!\n\n", segundo);
-                do{
-                printf("\nJogar novamente? (S/N): ");
-                scanf("%c", &reiniciar);  
-                setbuf(stdin, 0);
-                cont = reset(reiniciar, cont);
-                cleanmtrz(jogo);
-                }while (cont == 99);
+                cont++;
+                print_tabela(jogo, user1, pont1, pont2, user2); // function que imprime o tabela do jogo
+                if (cont != 11)
+                {
+                    do
+                    {
+                        if (cont % 2 != 0)
+                        {
+                            printf("\n\tVez de %s (X). \n", user1);
+                        }
+                        else
+                        {
+                            printf("\n\tVez de %s (O).\n", user2);
+                        }
+                        do
+                        {
+                            printf("\tDigite a linha: ");
+                            scanf("%d", &i);
+                            setbuf(stdin, 0);
+                        } while ((i > 3) || (i < 1));
+                        do
+                        {
+                            printf("\tDigite a coluna: ");
+                            scanf("%d", &j);
+                            setbuf(stdin, 0);
+                        } while ((j > 3) || (j < 1));
+
+                        if (cont % 2 != 0)
+                        {
+                            if (jogo[i - 1][j - 1] == ' ')
+                            {
+                                jogo[i - 1][j - 1] = 'X';
+                                controle = 0;
+                                system("cls");
+                            }
+                            else
+                            {
+                                printf("\n\tEsta posicao ja foi preenchida.\n");
+                                controle = 1;
+                            }
+                        }
+                        else
+                        {
+                            if (jogo[i - 1][j - 1] == ' ')
+                            {
+                                jogo[i - 1][j - 1] = 'O';
+                                controle = 0;
+                                system("cls");
+                            }
+                            else
+                            {
+                                printf("\n\tEsta posicao ja foi preenchida.\n");
+                                controle = 1;
+                            }
+                        }
+                    } while (controle == 1);
+                }
                 
+                if (cont > 4)
+                {
+                    vencedor = resultado(jogo);
+                    if (vencedor == 1)
+                    {
+                        pont1++;
+                        print_tabela(jogo, user1, pont1, pont2, user2);
+                        printf("\n\tFIM DE JOGO.\n\t%s VENCEU!!\n\n", user1);
+                        do
+                        {
+                            printf("\n\tJogar novamente? (S/N): ");
+                            scanf("%c", &reiniciar);
+                            setbuf(stdin, 0);
+                            cont = reset(reiniciar, cont); // function para reiniciar ou encerrar o jogo
+                            cleanmtrz(jogo);
+                        } while (cont == 99);
+                    }
+                    if (vencedor == 2)
+                    {
+                        pont2++;
+                        print_tabela(jogo, user1, pont1, pont2, user2);
+                        printf("\n\tFIM DE JOGO.\n\t%s VENCEU!!\n\n", user2);
+                        do
+                        {
+                            printf("\n\tJogar novamente? (S/N): ");
+                            scanf("%c", &reiniciar);
+                            setbuf(stdin, 0);
+                            cont = reset(reiniciar, cont);
+                            cleanmtrz(jogo);
+                        } while (cont == 99);
+                    }
+                }
+                // verifica se houve empate
+                if (cont == 9)
+                {
+                    print_tabela(jogo, user1, pont1, pont2, user2);
+                    printf("\n\tFIM DE JOGO, EMPATE.\n");
+                    do
+                    {
+                        printf("\n\tJogar novamente? (S/N): ");
+                        scanf("%c", &reiniciar);
+                        setbuf(stdin, 0);
+                        cont = reset(reiniciar, cont);
+                        cleanmtrz(jogo);
+                    } while (cont == 99);
+                }
             }
-        }   
-		//verifica se houve empate        
-        if(cont == 9){ 
-            print_tabela(jogo, primeiro,pont1,pont2,segundo);
-            printf("\nFIM DE JOGO, EMPATE.\n");
-            do{
-                printf("\nJogar novamente? (S/N): ");
-                scanf("%c", &reiniciar);
-                setbuf(stdin, 0);  
-                cont = reset(reiniciar, cont);
-                cleanmtrz(jogo);
-            }while (cont == 99);
-        }       
-    }
+
+            break;
+        case 2:
+            pont1 = 0;
+            pont2 = 0;
+            cleanmtrz(jogo); // function para limpar a matriz
+            printf("\n\tDigite o nome do jogador: ");
+            scanf(" %s", user1);
+            setbuf(stdin, 0);
+
+            printf("\tVoce jogara com (X). \n");
+            system("pause");
+            system("cls");
+
+            for (cont = 0; cont < 9;)
+            {
+                cont++;
+                print_tabela(jogo, user1, pont1, pont2, user3); // function que imprime o tabela do jogo
+
+                if (cont != 11)
+                {
+                    if (cont % 2 != 0)
+                    {
+                        printf("\n\tSua vez (X). \n");
+                        do
+                        {
+                            do
+                            {
+                                printf("\tDigite a linha: ");
+                                scanf("%d", &i);
+                                setbuf(stdin, 0);
+                            } while ((i > 3) || (i < 1));
+
+                            do
+                            {
+                                printf("\tDigite a coluna: ");
+                                scanf("%d", &j);
+                                setbuf(stdin, 0);
+                            } while ((j > 3) || (j < 1));
+
+                            if (jogo[i - 1][j - 1] == ' ')
+                            {
+                                jogo[i - 1][j - 1] = 'X';
+                                controle = 0;
+                                system("cls");
+                            }
+                            else
+                            {
+                                printf("\n\tEsta posicao ja foi preenchida.\n");
+                                controle = 1;
+                            }
+                        } while (controle == 1);
+                    }
+                    else
+                    {
+                        printf("\t\nProcessando...");
+                        do
+                        {
+                            srand(time(NULL));
+                            i = (rand() % 3);
+                            j = (rand() % 3);
+
+                            if (jogo[i][j] == ' ')
+                            {
+                                jogo[i][j] = 'O';
+                                controle = 0;
+                                system("cls");
+                            }
+                            else
+                            {
+                                controle = 1;
+                            }
+                        } while (controle == 1);
+                    }
+                }
+                if (cont > 4)
+                {
+                    vencedor = resultado(jogo);
+                    if (vencedor == 1)
+                    {
+                        pont1++;
+                        print_tabela(jogo, user1, pont1, pont2, user3);
+                        printf("\n\tFIM DE JOGO.\n\t%s VENCEU!!\n\n", user1);
+                        do
+                        {
+                            printf("\n\tJogar novamente? (S/N): ");
+                            scanf("%c", &reiniciar);
+                            setbuf(stdin, 0);
+                            cont = reset(reiniciar, cont); // function para reiniciar ou encerrar o jogo
+                            cleanmtrz(jogo);
+                        } while (cont == 99);
+                    }
+                    vencedor = resultado(jogo);
+                    if (vencedor == 2)
+                    {
+                        pont2++;
+                        print_tabela(jogo, user1, pont1, pont2, user3);
+                        printf("\n\tFIM DE JOGO.\n\tO Computador VENCEU!!\n\n");
+                        do
+                        {
+                            printf("\n\tJogar novamente? (S/N): ");
+                            scanf("%c", &reiniciar);
+                            setbuf(stdin, 0);
+                            cont = reset(reiniciar, cont);
+                            cleanmtrz(jogo);
+                        } while (cont == 99);
+                    }
+                }
+                // verifica se houve empate
+                if (cont == 9)
+                {
+                    print_tabela(jogo, user1, pont1, pont2, user3);
+                    printf("\n\tFIM DE JOGO, EMPATE.\n");
+                    do
+                    {
+                        printf("\n\tJogar novamente? (S/N): ");
+                        scanf("%c", &reiniciar);
+                        setbuf(stdin, 0);
+                        cont = reset(reiniciar, cont);
+                        cleanmtrz(jogo);
+                    } while (cont == 99);
+                }
+            }
+
+            break;
+        case 3:
+            exit(1);
+        default:
+            printf("\n\tOpcao invalida\n");
+            system("pause");
+            system("cls");
+        }
+    } while (1);
 }
